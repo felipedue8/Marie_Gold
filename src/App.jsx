523135ca8be40041d727895dfcc0780b11aee94a
felipedue8 <felipedue80@gmail.com>
2025-02-12 14:17:04 -0500

    se agrega productos de temporada

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React,{useRef, useEffect} from 'react';
-import './App.css'
 import dere from "./imgs/dere.png";
 import izqui from "./imgs/izqu.png";
+import { tarjetas } from './Tarjetas';
+
 
 export  function App() {
    const contenedorRef = useRef(null);
@@ -50,10 +51,13 @@ export  function App() {
   return (
     <>
       <header>
-      <img src="https://www.dropbox.com/scl/fi/vc6ba56dfr7e1apf10ia8/IMG-20230409-WA0005-removebg-preview.png?rlkey=olbdtfl8tdzb687zx1imshtla&st=kmydvt30&raw=1" alt="" />
-      <h1>Marie Golden</h1>
+      <img src="/imgs/logoPrincipal.jpg" alt="Logo" />
+
+      <h1>Marie Golden</h1> 
+      
       </header>
       <nav>
+
           <ul>
             <li>Mainllas</li>
             <li>Aretes</li>
@@ -70,21 +74,72 @@ export  function App() {
       <img onClick={clickdr} className='dr' src={dere} alt="" />
          
         </div>
+         
       <div ref={contenedorRef} className='fondo_imagenes'>
-      <img src="https://www.dropbox.com/scl/fi/0ynupo162tg32a5pylp70/IMG_20240921_164430.jpg?rlkey=b6rath9h5xjm63dcepsqfqgwz&st=ldnstxtj&raw=1" alt=""/>
-      <img src="https://www.dropbox.com/scl/fi/9qmbcs6p3bi1mipx0r61u/IMG_20240928_103556.jpg?rlkey=rsxzwmi05rbiaug2h9sqk562p&st=5uhdc2br&raw=1" alt="Imagen desde Dropbox"/>
-      <img src="https://www.dropbox.com/scl/fi/ppcjh5r00rr6blzmskx6u/IMG_20241129_154458.jpg?rlkey=1r5ruocvw6mnwvd5tz0qsgyp0&st=lug3u4p8&raw=1" alt="Imagen desde Dropbox"/>
-      <img src="https://www.dropbox.com/scl/fi/qjo1yjlh038zir5umf0ul/IMG_20241130_190716.jpg?rlkey=486jbp6iq5oct5ima45ez8cx1&st=9xel5w3h&raw=1" alt="Imagen desde Dropbox"/>
-      <img src="https://www.dropbox.com/scl/fi/80mlabem0toj6mh37cu8x/IMG_20241217_114723.jpg?rlkey=1dizzzuztpi2wzssaych31iz5&st=l0x3xd03&raw=1" alt="Imagen desde Dropbox"/>
-      <img src="https://www.dropbox.com/scl/fi/4gquryrgt99poe48t9chq/IMG-20231130-WA0057.jpg?rlkey=9zkbve5e7v9i1hzl15cezxeeq&st=p94wpvtg&raw=1" alt="Imagen desde Dropbox"/>
-      <img src="https://www.dropbox.com/scl/fi/3mnwfl341q7o4hwd6qzd4/IMG_20241216_142135.jpg?rlkey=hwppv98qhrzvdl9oyd7ic6drz&st=zuzwt22z&raw=1" alt="Imagen desde Dropbox"/>
-      <img src="https://www.dropbox.com/scl/fi/0tgyg0f5ltqxo54xdc868/IMG_20241010_162433.jpg?rlkey=qperxuqtdrmfqg17bucxgqs6w&st=z9qxbtwc&raw=1" alt="Imagen desde Dropbox"/>
+      
+        <img src="/ramos/cajita_con_rosas_eternas_con_mensaje_y_fotos.webp" alt="joyas" />
+        <img src="/ramos/cajita_con_rosas_eternas_y_peluche.webp" alt="joyas" />
+        <img src="/ramos/cajita_con_tapa_de_rosas_eternas.webp" alt="joyas" />
+        <img src="/ramos/cajita_con_tapa_rosas_eternas.webp" alt="joyas" />
+        <img src="/ramos/cajita_con_tapa, rosas_eternas_y_peluche.webp" alt="joyas" />
+        <img src="/ramos/cajita_de_rosas_eternas_con_ferrero_rocher.webp" alt="joyas" />
+        <img src="/ramos/cajita_de_rosas_eternas_con_hotwhells.webp" alt="joyas" />
+        <img src="/ramos/Ramo rosas azules .webp" alt="joyas" />
+        <img src="/ramos/Ramo seis rosa azules carros.webp" alt="joyas" />
 
-      <img src="https://www.dropbox.com/scl/fi/k91o8lb7x2eptirlsgvnn/IMG_20241212_103849.jpg?rlkey=orb46i55rxbizs0pfi367igu7&st=lc3lu2oq&raw=1" alt="" />
       </div>
       </div>
+      <div className='productos_temporada_titulo'> 
+      <h2>AHORA EN SAN VALENTIN</h2>
+      
+        <div className='productos_temporada'>
+            {tarjetas("ramo de flores + ferrero","ramo de flores de diferentes colores combinado con cholate ferrero", "60.000","/imagenes_ramos/Ramo_rosas_azules.jpg","Ramo de rosas azules")}
+            {tarjetas("Girasol en cajita","Girasol en cajita con tarjeta personalizada", "20.000","/peluches/girasol_en_cajita_con_peluche_pequeño.webp","Girasol en cajita")}
+            {tarjetas("Osito graduado","Peluche de oso graduado", "30.000","/peluches/osito_graduado_mediano.webp","Peluche de oso graduado")}
+            {tarjetas("Peluche graduado mediano","peluche graduado", "30.000","/peluches/peluche_graduado_mediano-8.webp","Peluche graduado mediano")}
+            {tarjetas("Peluche graduado grande","peluche graduado", "50.000","/peluches/peluche_mediano_graduado-4.webp","Peluche graduado grande")}
+            {tarjetas("Peluche graduado con tematica","peluche graduado", "50.000","/peluches/ramo_mediano_con_peluche_tematica_de_grado.webp","Peluche graduado grande")}
+            
+
+        </div>
+      </div>
+      <footer>
+      <div className='footer_izquierda'>
+        <div className='logo_descripcion'>
+          <img src="/imgs/vaca_dorada_footer.png" alt="logo Marie Golden" />
+          <div className='descripcion'>
+          <h3>Marie Golden</h3>
+          <p>En Marie Golden, la pasión y la creatividad se entrelazan para crear joyas y accesorios únicos que enamoran. Cada pieza es una obra de arte elaborada a mano con materiales de alta calidad, transmitiendo emociones y sentimientos que te conectarán con su alma. Nuestras joyas y accesorios son extensiones de tu personalidad, narrativas que cuentan tu historia y celebran tu esencia. </p>
+          </div>
+        </div>
+        <div className='redes_sociales'>
+          <a href="https://wa.me/+573227878174?text=Vi%20tu%20pagina%20web%20y%20me%20interesa%20tener%20mas%20informacion%20sobre%20tus%20productos">
+          <img src="/imgs/whatsapp_footer.png" alt="logo whatsapp" />
+          <span className='info_redes'>+57 322 7878174</span>
+          </a>
+          <a href="https://www.instagram.com/mar.iegolden?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
+          <img src="/imgs/instagram_footer.png" alt="logo instagram" />
+          <span className='info_redes'>mar.iegolden</span>
+          </a>
+          <a href="https://www.tiktok.com/@mariegolden27?is_from_webapp=1&sender_device=pc">
+          <img src="/imgs/tik_tok_footer.png" alt="logo tiktok" />
+          <span className='info_redes'>@mariegolden </span>
+
+          </a>
+         </div>
+      </div>
+      
+      
+
+     
+      
+
+      </footer>
     </>
+
+  
   )
+  
  
   
 }
