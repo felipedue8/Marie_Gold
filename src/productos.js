0d8bf3b93f4d92be973372dfae07c94012e5edf2
Juan Felipe Dueñas Barreto <90742202+felipedue8@users.noreply.github.com>
2025-08-08 15:14:45 -0500

    🛍️ Actualizar productos desde panel admin

--- src/productos.js
+++ src/productos.js
@@ -1,160 +1,100 @@
 
 
 export const productos = [
-  {
-    id: 3.11,
-    titulo: "Osito graduado",
-    descripcion: "Peluche de oso graduado en traje de graduación",
-    precio: "40.000",
-    imagen: "/3.11.webp",
-    alt: "Peluche de oso graduado"
-  },
-  {
-    id: 3.12,
-    titulo: "Osito graduado Con Girasol",
-    descripcion: "Peluche de oso graduado con girasol",
-    precio: "50.000",
-    imagen: "/3.12.webp",
-    alt: "Peluche de oso graduado con girasol"
-  },
-  {
-    id: 3.13,
-    titulo: "Perrito graduado",
-    descripcion: "Peluche de perrito graduado",
-    precio: "45.000",
-    imagen: "/3.13.webp",
-    alt: "Peluche de perrito graduado"
-  },
-  /*
     {
-      id: 3.13,
-      titulo: "Perrito graduado",
-      descripcion: "Peluche de perrito graduado",
-      precio: "45.000",
-      imagen: "/3.13.webp",
-      alt: "Peluche de perrito graduado"
+      "id": 3.11,
+      "titulo": "Osito graduado",
+      "descripcion": "Peluche de oso graduado en traje de graduación",
+      "precio": "40.000",
+      "imagen": "/3.11.webp",
+      "alt": "Peluche de oso graduado"
     },
-  */
-  /*
     {
-      id: 3.13,
-      titulo: "Perrito graduado",
-      descripcion: "Peluche de perrito graduado",
-      precio: "45.000",
-      imagen: "/3.13.webp",
-      alt: "Peluche de perrito graduado"
+      "id": 3.12,
+      "titulo": "Osito graduado Con Girasol",
+      "descripcion": "Peluche de oso graduado con girasol",
+      "precio": "50.000",
+      "imagen": "/3.12.webp",
+      "alt": "Peluche de oso graduado con girasol"
     },
-  */
-  {
-    id: 3.16,
-    titulo: "Osito graduado Con rosa",
-    descripcion: "Peluche de oso graduado con rosa",
-    precio: "45.000",
-    imagen: "/3.16.webp",
-    alt: "Peluche de oso graduado con rosa"
-  },
-  /*
-   {
-    id: 3.17,
-    titulo: "Osito graduado",
-    descripcion: "Peluche de oso graduado",
-    precio: "30.000",
-    imagen: "/3.17.webp",
-    alt: "Peluche de oso graduado"
-  },
-  */
-  {
-    id: 3.18,
-    titulo: "Osito pequeño con rosa en cajita",
-    descripcion: "Peluche de oso pequeño con rosa en cajita",
-    precio: "20.000",
-    imagen: "/3.18.webp",
-    alt: "Peluche de oso pequeño con rosa en cajita"
-  },
-  {
-    id: 3.19,
-    titulo: "Osito blanco graduado con diploma",
-    descripcion: "Peluche de oso blanco graduado con diploma",
-    precio: "50.000",
-    imagen: "/3.19.webp",
-    alt: "Peluche de oso blanco graduado con diploma"
-  },
-  {
-    id: 4.11,
-    titulo: "Ramo de 5 rosas + hotwheels",
-    descripcion: "Ramo de 5 rosas + hotwheels + ferrero",
-    precio: "50.000",
-    imagen: "/4.12.webp",
-    alt: "Ramo de flores + hotwheels"
-  },
-  {
-    id: 4.12,
-    titulo: "Ramo de 8 rosas personalizado",
-    descripcion: "Ramo de 8 rosas personalizado",
-    precio: "40.000",
-    imagen: "/4.12.webp",
-    alt: "Ramo de 8 rosas personalizado"
-  },
-  {
-    id: 4.13,
-    titulo: "Caja en Forma de Corazón con Rosas Eternas",
-    descripcion: "Caja en forma de corazón con rosas eternas y peluche",
-    precio: "25.000",
-    imagen: "/4.13.webp",
-    alt: "Caja en Forma de Corazón con Rosas Eternas"
-  },
-  /*
     {
-    id: 4.14,
-    titulo: "Ramo de flores + hotwheels",
-    descripcion: "ramo de flores de diferentes colores combinado con hotwheels",
-    precio: "30.000",
-    imagen: "/4.14.webp",
-    alt: "Ramo de flores + hotwheels"
-  },
-  */
-  {
-    id: 4.15,
-    titulo: "Caja sorpresa con rosas eternas, mensaje y fotos",
-    descripcion: "Caja sorpresa con rosas eternas, mensaje y fotos",
-    precio: "40.000",
-    imagen: "/4.15.webp",
-    alt: "Caja sorpresa con rosas eternas, mensaje y fotos"
-  },
-  /*
-    {
-      id: 4.16,
-      titulo: "Ramo de flores + hotwheels",
-      descripcion: "ramo de flores de diferentes colores combinado con hotwheels",
-      precio: "30.000",
-      imagen: "/4.16.webp",
-      alt: "Ramo de flores + hotwheels"
+      "id": 3.13,
+      "titulo": "Perrito graduado con sombrero",
+      "descripcion": "Peluche de perrito graduado",
+      "precio": "45.000",
+      "imagen": "/3.13.webp",
+      "alt": "Peluche de perrito graduado"
     },
-    */
-  {
-    id: 4.17,
-    titulo: "Caja en forma de corazon con 10 rosas y ferrero",
-    descripcion: "Caja en forma de corazón con 10 rosas y ferrero",
-    precio: "60.000",
-    imagen: "/4.17.webp",
-    alt: "Caja en forma de corazón con 10 rosas y ferrero"
-  },
-  /*
     {
-    id: 4.18,
-    titulo: "Ramo de flores + hotwheels",
-    descripcion: "ramo de flores de diferentes colores combinado con hotwheels",
-    precio: "30.000",
-    imagen: "/4.18.webp",
-    alt: "Ramo de flores + hotwheels"
-  },
-  */
-  {
-    id: 4.19,
-    titulo: "Ramo de flores + hotwheels",
-    descripcion: "ramo de flores de diferentes colores combinado con hotwheels",
-    precio: "50.000",
-    imagen: "/4.19.webp",
-    alt: "Ramo de flores + hotwheels"
-  },
-];
+      "id": 3.16,
+      "titulo": "Osito graduado Con rosa",
+      "descripcion": "Peluche de oso graduado con rosa",
+      "precio": "45.000",
+      "imagen": "/3.16.webp",
+      "alt": "Peluche de oso graduado con rosa"
+    },
+    {
+      "id": 3.18,
+      "titulo": "Osito pequeño con rosa en cajita",
+      "descripcion": "Peluche de oso pequeño con rosa en cajita",
+      "precio": "20.000",
+      "imagen": "/3.18.webp",
+      "alt": "Peluche de oso pequeño con rosa en cajita"
+    },
+    {
+      "id": 3.19,
+      "titulo": "Osito blanco graduado con diploma",
+      "descripcion": "Peluche de oso blanco graduado con diploma",
+      "precio": "50.000",
+      "imagen": "/3.19.webp",
+      "alt": "Peluche de oso blanco graduado con diploma"
+    },
+    {
+      "id": 4.11,
+      "titulo": "Ramo de 5 rosas + hotwheels",
+      "descripcion": "Ramo de 5 rosas + hotwheels + ferrero",
+      "precio": "50.000",
+      "imagen": "/4.12.webp",
+      "alt": "Ramo de flores + hotwheels"
+    },
+    {
+      "id": 4.12,
+      "titulo": "Ramo de 8 rosas personalizado",
+      "descripcion": "Ramo de 8 rosas personalizado",
+      "precio": "40.000",
+      "imagen": "/4.12.webp",
+      "alt": "Ramo de 8 rosas personalizado"
+    },
+    {
+      "id": 4.13,
+      "titulo": "Caja en Forma de Corazón con Rosas Eternas",
+      "descripcion": "Caja en forma de corazón con rosas eternas y peluche",
+      "precio": "25.000",
+      "imagen": "/4.13.webp",
+      "alt": "Caja en Forma de Corazón con Rosas Eternas"
+    },
+    {
+      "id": 4.15,
+      "titulo": "Caja sorpresa con rosas eternas, mensaje y fotos",
+      "descripcion": "Caja sorpresa con rosas eternas, mensaje y fotos",
+      "precio": "40.000",
+      "imagen": "/4.15.webp",
+      "alt": "Caja sorpresa con rosas eternas, mensaje y fotos"
+    },
+    {
+      "id": 4.17,
+      "titulo": "Caja en forma de corazon con 10 rosas y ferrero",
+      "descripcion": "Caja en forma de corazón con 10 rosas y ferrero",
+      "precio": "60.000",
+      "imagen": "/4.17.webp",
+      "alt": "Caja en forma de corazón con 10 rosas y ferrero"
+    },
+    {
+      "id": 4.19,
+      "titulo": "Ramo de flores + hotwheels",
+      "descripcion": "ramo de flores de diferentes colores combinado con hotwheels",
+      "precio": "50.000",
+      "imagen": "/4.19.webp",
+      "alt": "Ramo de flores + hotwheels"
+    }
+  ];
