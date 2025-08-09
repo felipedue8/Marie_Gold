9ce2df70260822541d8f82731e105a8c953fe405
Juan Felipe Dueñas Barreto <90742202+felipedue8@users.noreply.github.com>
2025-08-09 11:08:31 -0500

    � Actualizar titulo del producto 4.11: "Ramo de 5 rosas + hotwheels " → "Ramo de 5 rosas + hotwheels A"

--- src/productos.js
+++ src/productos.js
@@ -4,7 +4,7 @@ export const productos = [
     {
       "id": 3.11,
       "titulo": "Osito graduado",
-      "descripcion": "Peluche de oso graduado en traje de graduaciÃÂÃÂ³n",
+      "descripcion": "Peluche de oso graduado en traje de graduaciÃÂÃÂÃÂÃÂ³n",
       "precio": "40.000",
       "imagen": "/3.11.webp",
       "alt": "Peluche de oso graduado"
@@ -35,11 +35,11 @@ export const productos = [
     },
     {
       "id": 3.18,
-      "titulo": "Osito pequeÃÂÃÂ±o con rosa en cajita",
-      "descripcion": "Peluche de oso pequeÃÂÃÂ±o con rosa en cajita",
+      "titulo": "Osito pequeÃÂÃÂÃÂÃÂ±o con rosa en cajita",
+      "descripcion": "Peluche de oso pequeÃÂÃÂÃÂÃÂ±o con rosa en cajita",
       "precio": "20.000",
       "imagen": "/3.18.webp",
-      "alt": "Peluche de oso pequeÃÂÃÂ±o con rosa en cajita"
+      "alt": "Peluche de oso pequeÃÂÃÂÃÂÃÂ±o con rosa en cajita"
     },
     {
       "id": 3.19,
@@ -51,7 +51,7 @@ export const productos = [
     },
     {
       "id": 4.11,
-      "titulo": "Ramo de 5 rosas + hotwheels ",
+      "titulo": "Ramo de 5 rosas + hotwheels A",
       "descripcion": "Ramo de 5 rosas + hotwheels + ferrero roche",
       "precio": "50.000",
       "imagen": "/4.12.webp",
@@ -67,11 +67,11 @@ export const productos = [
     },
     {
       "id": 4.13,
-      "titulo": "Caja en Forma de CorazÃÂÃÂ³n con Rosas Eternas",
-      "descripcion": "Caja en forma de corazÃÂÃÂ³n con rosas eternas y peluche",
+      "titulo": "Caja en Forma de CorazÃÂÃÂÃÂÃÂ³n con Rosas Eternas",
+      "descripcion": "Caja en forma de corazÃÂÃÂÃÂÃÂ³n con rosas eternas y peluche",
       "precio": "25.000",
       "imagen": "/4.13.webp",
-      "alt": "Caja en Forma de CorazÃÂÃÂ³n con Rosas Eternas"
+      "alt": "Caja en Forma de CorazÃÂÃÂÃÂÃÂ³n con Rosas Eternas"
     },
     {
       "id": 4.15,
@@ -84,10 +84,10 @@ export const productos = [
     {
       "id": 4.17,
       "titulo": "Caja en forma de corazon con 10 rosas y ferrero",
-      "descripcion": "Caja en forma de corazÃÂÃÂ³n con 10 rosas y ferrero",
+      "descripcion": "Caja en forma de corazÃÂÃÂÃÂÃÂ³n con 10 rosas y ferrero",
       "precio": "60.000",
       "imagen": "/4.17.webp",
-      "alt": "Caja en forma de corazÃÂÃÂ³n con 10 rosas y ferrero"
+      "alt": "Caja en forma de corazÃÂÃÂÃÂÃÂ³n con 10 rosas y ferrero"
     },
     {
       "id": 4.19,
